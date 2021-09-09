1f0e4102416e605225e66a68480726ae730f44ac
dm82m <dirk@maucher-online.de>
2021-09-09 16:43:39 +0200

    differentiating between state_class measurement and total_increasing

--- custom_components/wemportal/sensor.py
+++ custom_components/wemportal/sensor.py
@@ -18,7 +18,7 @@ from datetime import timedelta
 import async_timeout
 import homeassistant.helpers.config_validation as config_validation
 import voluptuous as vol
-from homeassistant.components.sensor import SensorEntity, PLATFORM_SCHEMA, STATE_CLASS_MEASUREMENT
+from homeassistant.components.sensor import SensorEntity, PLATFORM_SCHEMA, STATE_CLASS_MEASUREMENT, STATE_CLASS_TOTAL_INCREASING
 from homeassistant.const import (
     CONF_USERNAME,
     CONF_PASSWORD,
@@ -180,8 +180,10 @@ class WemPortalSensor(SensorEntity):
     @property
     def state_class(self):
         """Return the state class of this entity, if any."""
-        if self._unit == '°C' or self._unit == 'kWh' or self._unit == 'Wh' or self._unit == 'kW' or self._unit == 'W' or self._unit == '%':
+        if self._unit == '°C' or self._unit == 'kW' or self._unit == 'W' or self._unit == '%':
             return STATE_CLASS_MEASUREMENT
+        elif self._unit == 'kWh' or self._unit == 'Wh':
+            return STATE_CLASS_TOTAL_INCREASING
         else:
             return None
 
