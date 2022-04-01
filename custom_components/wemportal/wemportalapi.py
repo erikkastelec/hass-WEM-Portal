5471aed24ff073322121d934d73e0e5cddec6e87
Erik Kastelec <erikkastelec@gmail.com>
2022-04-01 11:53:37 +0200

    Fixed comma problem for german locale. Fixes #20

--- custom_components/wemportal/wemportalapi.py
+++ custom_components/wemportal/wemportalapi.py
@@ -427,7 +427,7 @@ class WemPortalSpider(Spider):
     def navigate_to_expert_page(self, response):
         # sleep for 1 seconds to get proper language and updated data
         time.sleep(1)
-        #_LOGGER.debug("Print user page HTML: %s", response.text)
+        # _LOGGER.debug("Print user page HTML: %s", response.text)
         if (
                 response.url
                 == "https://www.wemportal.com/Web/login.aspx?AspxAutoDetectCookieSupport=1"
@@ -480,7 +480,7 @@ class WemPortalSpider(Spider):
     def scrape_pages(self, response):
         # sleep for 1 seconds to get proper language and updated data
         time.sleep(1)
-        #_LOGGER.debug("Print expert page HTML: %s", response.text)
+        # _LOGGER.debug("Print expert page HTML: %s", response.text)
         if self.authErrorFlag:
             yield {"authErrorFlag": True}
         _LOGGER.debug("Scraping page")
@@ -527,7 +527,8 @@ class WemPortalSpider(Spider):
                     else:
                         value = split_value[0]
                     try:
-                        value = int(value)
+                        value = ".".join(value.split(","))
+                        value = float(value)
                     except ValueError:
                         if value == "--" or value == "off":
                             value = 0.0
