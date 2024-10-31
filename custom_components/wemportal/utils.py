9f74a2a9eda4d91d29ad8ec265ef62baf24c8ae9
Konstantin Baumann <konstantin.baumann@autodesk.com>
2024-10-31 08:12:27 +0100

    fix also the casing of some units: `KW` -> `kW`, `KWh` -> `kWh`

--- custom_components/wemportal/utils.py
+++ custom_components/wemportal/utils.py
@@ -1,7 +1,10 @@
 def fix_unit_of_measurement(uom: str) -> str:
-    """Fix the unit of measurement. WEM Portal uses "kW (W)" for "W" and "kW (W)h" for "Wh"."""
+    """Fix the unit of measurement. WEM Portal uses "kW (W)" for "W" and "kW (W)h" for "Wh". Also fix the casing of some units."""
 
     return {
-        "kW (W)": "W",
-        "kW (W)h": "Wh",
-    }.get(uom, uom)
+        "w": "W",
+        "kw": "kW",
+        "kwh": "kWh",
+        "kw (w)": "W",
+        "kw (w)h": "Wh",
+    }.get(uom.lower(), uom)
